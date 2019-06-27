360bb14c6715b20fff905721398cdaedbbc79e3b
alp69799 <alp69799@hanyang.ac.kr>
2019-06-27 15:53:19 +0900

    ADD DBCONFIG

---
+++ DBConfig/DBConfig.php
@@ -0,0 +1,27 @@
+<?php
+
+define("DB_HOST", "localhost");
+define("USER_NAME", "*******");
+define("PASS", "*******");
+define("DB_NAME", "*******");
+
+$_CONN = mysqli_connect(DB_HOST, USER_NAME, PASS, DB_NAME); //DataBase Connection
+
+//Set database's charset to utf-8
+$STMT = $_CONN->prepare("set session character_set_connection=utf8;");
+$STMT->execute();
+$STMT = $_CONN->prepare("set session character_set_results=utf8;");
+$STMT->execute();
+$STMT = $_CONN->prepare("set session character_set_client=utf8;");
+$STMT->execute();
+
+//Find exception and processing
+if(!$_CONN){
+  $jsonObj = array();
+  $jsonObj['HEAD'] = array();
+  $jsonObj['HEAD'] += ['ResCode' => 500, 'ResMsg' => "DB_CONNECTION_ERROR"];
+
+  echo json_encode($jsonObj); exit;
+}
+
+ ?>
--- apiControl.php
+++ apiControl.php
@@ -2,11 +2,11 @@
 header('Content-Type: application/json');
 require_once('./Message/message.php');
 
-$postJSON = json_decode(base64_decode(file_get_contents("php://input")), true);
+$postJSON = json_decode(file_get_contents("php://input"), true);
 
 switch($postJSON['query']){
-
+  
   default :
-    sendWrongRequestMsg(); break;
+    echo sendWrongRequestMsg(); break;
 }
  ?>
