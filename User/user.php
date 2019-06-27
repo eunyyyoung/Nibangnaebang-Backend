ca4fa33e7edf90d5abcae7954aaac8d2e927f821
alp69799 <alp69799@hanyang.ac.kr>
2019-06-27 18:10:51 +0900

    ADD USER_SIGNUP_FUNCTION

--- User/user.php
+++ User/user.php
@@ -28,4 +28,61 @@ function Login($originJSON){
   }
 }
 
+
+function SignUp($originJSON){
+
+  if(!(isset($originJSON['user']) && isset($originJSON['user']['id']) && isset($originJSON['user']['pwd']) && isset($originJSON['user']['gender'])
+   && isset($originJSON['user']['school']) && isset($originJSON['user']['token']))){
+    return sendWrongRequestMsg();
+  }
+
+  $jsonObj = array();
+  require_once('./DBConfig/DBConfig.php');
+
+  $STMT = $_CONN->prepare('SELECT COUNT(*) FROM NN_USER WHERE Id=?');
+  @$STMT->bind_param("s", trim($originJSON['user']['id']));
+  $STMT->execute();
+  $RES = $STMT->get_result();
+
+  if($ROW = mysqli_fetch_assoc($RES)){
+    if($ROW['COUNT(*)'] == 1){
+      $jsonObj += [ 'ResCode' => 400, 'ResMsg' => 'Already_Exist_ID' ];
+      return json_encode($jsonObj);
+    }
+  }
+
+  if(!isset($_FILES['file'])){
+    return sendWrongRequestMsg();
+  }
+
+  require_once('./FileManage/fileManage.php');
+  $name = uploadFile();
+
+  $STMT = $_CONN -> prepare('INSERT INTO NN_USER(Id, Pwd, Gender, School, AuthOk, Token, IdCardDir) VALUES(?,?,?,?,0,?,?)');
+  @$STMT->bind_param('sssiss',  trim($originJSON['user']['id']),
+                               trim($originJSON['user']['pwd']),
+                               trim($originJSON['user']['gender']),
+                               $originJSON['user']['school'],
+                               trim($originJSON['user']['token']),
+                               trim($name));
+
+
+  $STMT->execute();
+
+  $STMT = $_CONN->prepare('SELECT COUNT(*) FROM NN_USER WHERE Id=?');
+  @$STMT->bind_param("s", trim($originJSON['user']['id']));
+  $STMT->execute();
+  $RES = $STMT->get_result();
+
+  if($ROW = mysqli_fetch_assoc($RES)){
+    if($ROW['COUNT(*)'] == 1){
+      $jsonObj += ['ResCode' => 200, 'ResMsg' => 'Success'];
+      return json_encode($jsonObj);
+    }else {
+      $jsonObj += ['ResCode' => 200, 'ResMsg' => 'Fail'];
+      return json_encode($jsonObj);
+    }
+  }
+}
+
  ?>
--- apiControl.php
+++ apiControl.php
@@ -1,13 +1,20 @@
 <?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
 header('Content-Type: application/json');
 require_once('./Message/message.php');
 
-$postJSON = json_decode(file_get_contents("php://input"), true);
+$postJSON = json_decode($_POST['jsonString'],true);
 
 switch($postJSON['query']){
   case 'Login':
     require_once('./User/user.php');
     echo Login($postJSON);
+    break;
+  case 'SignUp' :
+    require_once('./User/user.php');
+    echo SignUp($postJSON);
     break;
   default :
     echo sendWrongRequestMsg(); break;
