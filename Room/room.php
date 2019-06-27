95355d2a5d2dce2307ad30d5737c808cd85364e0
eunyyyoung <any218@hanmail.net>
2019-06-28 01:22:42 +0900

    update showRoomList, findARoom,  createSellRoom API

--- Room/room.php
+++ Room/room.php
@@ -1,46 +1,73 @@
 <?php
 
 function showRoomList(){
-    
+
     require_once('./DBConfig/DBConfig.php');
 
-    $STMT = $_CONN->prepare('SELECT * FROM NN_ROOM');
+    $STMT = $_CONN->prepare('SELECT No, Pay, ALStart, ALEnd, Title FROM NN_ROOM Where IsView = 0 ORDER BY LogDate DESC');
     $STMT->execute();
     $RES = $STMT->get_result();
 
+    $jsonObj = array();
+
+    while($ROW = mysqli_fetch_assoc($RES)){
+      array_push($jsonObj, $ROW);
+    }
+
+    return json_encode($jsonObj);
 }
 
 function findARoom($originJSON){
-
     require_once('./DBConfig/DBConfig.php');
-
     $STMT = $_CONN->prepare('SELECT * FROM NN_ROOM WHERE No=?');
-    @$STMT->bind_param("i", trim($originJSON['room']['RoomNo']));
+    @$STMT->bind_param("i",$originJSON['RoomNo']);
     $STMT->execute();
     $RES = $STMT->get_result();
 
-    return json_encode($RES);
+    $ROW = mysqli_fetch_assoc($RES);
+
+    return json_encode($ROW);
 }
 
 function createSellRoom($originJSON){
     $nowDate = date("Y-m-d/H:i:s");
 
-    $STMT = $_CONN->prepare("INSERT INTO NN_ROOM(ALEnd, ALStart, Pay, Address, Title, Detail, IsLoad, LogDate) VALUES(?,?,0,?,?,?,0,?)");
-    @$STMT->bind_param("ssisssis", trim($postJSON['room']['ALStart']), 
-                                    trim($postJSON['room']['ALEnd']),
-                                    $postJSON['room']['pay'],
-                                    trim($postJSON['room']['address']),
-                                    trim($postJSON['room']['title']),
-                                    trim($postJSON['room']['detail']),
-                                    $postJSON['room']['isLoad'],
-                                    trim($nowDate));
+    require_once('./DBConfig/DBConfig.php');
+    
+    $STMT = $_CONN->prepare("INSERT INTO NN_ROOM(ALStart, ALEnd, Pay, Address, Title, Detail, IsLoad, Seller, IsView, Lat, Lng ,School, LogDate, PayType) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+    @$STMT->bind_param("ssisssiiiddsss", trim($originJSON['room']['ALStart']),
+                                    trim($originJSON['room']['ALEnd']),
+                                    $originJSON['room']['pay'],
+                                    trim($originJSON['room']['address']),
+                                    trim($originJSON['room']['title']),
+                                    trim($originJSON['room']['detail']),
+                                    $originJSON['room']['isLoad'],
+                                    $originJSON['room']['userNo'],
+                                    $originJSON['room']['isView'],
+                                    $originJSON['room']['Lat'],
+                                    $originJSON['room']['Lng'],
+                                    trim($originJSON['room']['school']),
+                                    trim($nowDate),
+                                    trim($originJSON['room']['payType']));
     $STMT->execute();
-
+    $RES = $STMT->get_result();
     $jsonObj = array();
 
-    $result = findARoom();
-    $jsonObj += [ 'code' => 'success', 'RoomNo' => $result['No']];
-        
+    if($RES == false){
+        $jsonObj += [ 'code' => 'success', 'RoomNo' => mysqli_insert_id($_CONN)];
+
+        require_once('./FileManage/fileManage.php');
+        $names = uploadFile();
+
+        foreach($names as $name){
+          $STMT = $_CONN->prepare("INSERT INTO NN_ROOMIMG(RoomNo) VALUES(?)");
+          @$STMT->bind_param("i", mysqli_insert_id($_CONN));
+          $STMT->execute();
+        }
+
+    }else{
+        $jsonObj += [ 'code' => 'error', 'msg' => 'create sell room Fail'];
+    }
     return json_encode($jsonObj);
 }
 
