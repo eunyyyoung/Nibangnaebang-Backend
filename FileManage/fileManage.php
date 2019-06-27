f0b7a48ac7b61500207d91dc04b8bc4cba31b0ba
alp69799 <alp69799@hanyang.ac.kr>
2019-06-28 01:07:44 +0900

    MODIFY FILE_MANAGE

--- FileManage/fileManage.php
+++ FileManage/fileManage.php
@@ -1,7 +1,7 @@
 <?php
 
 function uploadFile(){
-  /*$uploads_dir = './Images';
+  $uploads_dir = './Images';
   $allowed_ext = array('jpg', 'jpeg', 'png', 'gif');
 
   $error = $_FILES['file']['error'];
@@ -34,42 +34,7 @@ function uploadFile(){
   // 파일 이동
   move_uploaded_file( $_FILES['file']['tmp_name'], "$uploads_dir/$name");
 
-  return $name; */
-
-  $uploads_dir = './Images/';
-  $allowed_ext = array('jpg', 'jpeg', 'png', 'gif');
-  $fileNames = array();
-
-  foreach($_FILES['file']['name'] as $f => $name){
-    $name = $_FILES['file']['name'][$f];
-
-    $uploadName = explode('.', $name);
-
-    $uploadname = time().$f.'.'.$uploadName[1];
-    $uploadFile = $uploads_dir.$uploadname;
-
-
-    if( $error != UPLOAD_ERR_OK ) {
-    	switch( $error ) {
-    		case UPLOAD_ERR_INI_SIZE:
-    		case UPLOAD_ERR_FORM_SIZE:
-    			return "ERROR";
-    			break;
-    		case UPLOAD_ERR_NO_FILE:
-    			return "ERROR";
-    			break;
-    		default:
-    			return "ERROR";
-    	}
-    	exit;
-    }
-
-    move_uploaded_file( $_FILES['file']['tmp_name'], $uploadFile);
-
-    array_push($fileNames, $uploadname);
-  }
-
-  return $fileNames;
+  return $name;
 }
 
  ?>
